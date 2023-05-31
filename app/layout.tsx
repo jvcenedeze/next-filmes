be33c879b4fd268e8c804853ef9c6f3df830dbf9
jvcenedeze <jvcenedeze@hotmail.com>
2023-05-31 19:54:52 -0300

    refact: using built-in next/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { Montserrat } from "@next/font/google";
+import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({
   weight: ["400", "700"],

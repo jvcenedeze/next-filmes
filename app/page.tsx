db0040f606efd366d15e1747c44c29029a90fcf9
jvcenedeze <jvcenedeze@hotmail.com>
2023-05-31 19:32:39 -0300

    refact: data type

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
 import Movie from "./Movie";
 
+interface MovieResults {
+  adult: Boolean;
+  backdrop_path: String;
+  genre_ids: Array<Number>;
+  id: React.Key;
+  original_language: String;
+  original_title: String;
+  overview: String;
+  popularity: Number;
+  poster_path: String;
+  release_date: String;
+  title: String;
+  video: Boolean;
+  vote_average: Number;
+  vote_count: Number;
+}
+
 export default async function Home() {
   const data = await fetch(
     `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
@@ -8,7 +25,7 @@ export default async function Home() {
   return (
     <main>
       <div className="grid gap-16 grid-cols-fluid">
-        {res.results.map((movie) => (
+        {res.results.map((movie: MovieResults) => (
           <Movie
             key={movie.id}
             id={movie.id}

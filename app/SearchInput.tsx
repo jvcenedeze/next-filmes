bf57cc014c24c6c3bb339320da92bba30e6fa846
jvcenedeze <jvcenedeze@hotmail.com>
2023-06-04 22:04:40 -0300

    feat: search input

---
+++ app/SearchInput.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Search() {
+  const [search, setSearch] = useState("");
+  return (
+    <div className="w-full relative inline-block flex items-center">
+      <input
+        type="text"
+        className="
+          w-full
+          border-2
+          py-2
+          px-3
+          rounded-lg
+          text-slate-700
+          text-black
+          focus:outline-none
+          focus:border-gray-300
+        "
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="absolute right-3 text-slate-700 cursor-pointer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="1em"
+          viewBox="0 0 512 512"
+          fill="currentColor"
+        >
+          <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+        </svg>
+      </div>
+    </div>
+  );
+}

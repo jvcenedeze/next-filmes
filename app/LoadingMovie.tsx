9360fc43758ebc07463ead3acbbe707402fb548b
jvcenedeze <jvcenedeze@hotmail.com>
2023-06-13 21:02:04 -0300

    feat: loading component for movie

---
+++ app/LoadingMovie.tsx
@@ -0,0 +1,9 @@
+export default function LoadingMovie() {
+  return (
+    <div className="flex-1">
+      <div className="bg-slate-200 h-4 w-3/4 mb-2 rounded"></div>
+      <div className="bg-slate-200 h-4 w-2/5 mb-2 rounded"></div>
+      <div className="bg-slate-200 aspect-2/3 rounded"></div>
+    </div>
+  );
+}

4852bcc0562236ef36e539788c88b139e52bb35b
jvcenedeze <jvcenedeze@hotmail.com>
2023-06-20 21:37:33 -0300

    feat: reset search value when changing route

--- app/SearchInput.tsx
+++ app/SearchInput.tsx
@@ -1,17 +1,21 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 export default function Search() {
   const [search, setSearch] = useState("");
   const router = useRouter();
+  const path = usePathname();
   const onSearch = (event: React.FormEvent) => {
     event.preventDefault();
     if (search === "") return;
     const encodedSearchQuery = encodeURI(search);
     router.push(`/search?q=${encodedSearchQuery}`);
   };
+  useEffect(() => {
+    if (path !== "/search") setSearch("");
+  }, [path]);
   return (
     <div className="w-full relative inline-block flex items-center">
       <form onSubmit={onSearch} className="w-full">

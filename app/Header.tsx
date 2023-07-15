d08eb2d0a69790a943dfd7418fecfc8b638bbbc9
jvcenedeze <jvcenedeze@hotmail.com>
2023-07-15 16:13:16 -0300

    refact: spacing

--- app/Header.tsx
+++ app/Header.tsx
@@ -11,17 +11,17 @@ const josefinSans = Josefin_Sans({
 
 export default function Header() {
   return (
-    <nav className="flex flex-row pt-4">
+    <nav className="flex flex-row py-1 sm:py-2 pl-16 sm:pl-0 top-0 right-0 left-0 fixed bg-black">
       <Link
-        className={`${josefinSans.className} mx-auto sm:mr-0 mt-auto sm:ml-16 lg:ml-32 text-4xl`}
+        className={`${josefinSans.className} ml-auto sm:ml-16 lg:ml-32 sm:mx-auto sm:mr-0 mt-auto text-2xl sm:text-4xl`}
         href="/"
       >
         Next filmes
       </Link>
-      <div className="ml-4 hidden sm:block max-w-xs w-2/5 lg:w-1/2">
+      <div className="ml-4 hidden sm:block max-w-xs w-1/3 lg:w-1/2">
         <SearchInput />
       </div>
-      <div className="ml-auto hidden sm:block sm:mr-16 lg:mr-32">
+      <div className="ml-auto mr-6 sm:mr-16 lg:mr-32">
         <HeaderMenu />
       </div>
     </nav>

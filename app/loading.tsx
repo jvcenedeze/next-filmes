cc7fb95439faa2c032e3fdea6785ec1b7174bad6
jvcenedeze <jvcenedeze@hotmail.com>
2023-07-15 16:27:18 -0300

    feat: loading state

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+import LoadingMovie from "./LoadingMovie";
+
+export default function LoadingHome() {
+  const loadingComponents = Array(20).fill("");
+  return (
+    <div className="animate-pulse grid gap-10 sm:gap-16 grid-cols-fluid">
+      {loadingComponents.map((_, i) => (
+        <LoadingMovie key={i} />
+      ))}
+    </div>
+  );
+}

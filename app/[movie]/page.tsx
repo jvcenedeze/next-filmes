7d6129c46c7ca9a2338b83dcd99e309b520297cf
jvcenedeze <jvcenedeze@hotmail.com>
2023-07-19 22:04:25 -0300

    feat: rating / runtime format

--- app/[movie]/page.tsx
+++ app/[movie]/page.tsx
@@ -1,17 +1,59 @@
 import Image from "next/image";
 import Link from "next/link";
+import Rating from "../../components/Rating";
+
+interface BelongsToCollection {
+  id: number;
+  name: string;
+  poster_path: string;
+  backdrop_path: string | null;
+}
+
+interface Genres {
+  id: number;
+  name: string;
+}
+
+interface ProductionCompanies {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+interface ProductionCountries {
+  iso_3166_1: string;
+  name: string;
+}
+
+interface SpokenLanguages {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
 
 interface ResResults {
   adult: boolean;
   backdrop_path: string;
-  genre_ids: Array<number>;
+  belongs_to_collection: BelongsToCollection;
+  budget: number;
+  genres: Array<Genres>;
+  homepage: string;
   id: number;
+  imdb_id: string;
   original_language: string;
   original_title: string;
   overview: string;
   popularity: number;
   poster_path: string;
+  production_companies: Array<ProductionCompanies>;
+  production_countries: Array<ProductionCountries>;
   release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: Array<SpokenLanguages>;
+  status: string;
+  tagline: string;
   title: string;
   video: boolean;
   vote_average: number;
@@ -36,12 +78,28 @@ export default async function MovieDetail({ params }) {
     { next: { revalidate: 60 } }
   );
   const res = await data.json();
+  const voteRatingNumber = res.vote_average / 2;
+  const formatedReleaseDate = new Date(res.release_date).toLocaleDateString(
+    "en-US",
+    {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    }
+  );
+  const formatedRuntime = () => {
+    const time = res.runtime;
+    const hours = Math.floor(time / 60);
+    const minutes = time % 60;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    return `${hours}h${formattedMinutes}m`;
+  };
   return (
     <div>
       <div>
         <h2 className="text-2xl">{res.title}</h2>
-        <h2 className="text-lg">{res.release_date}</h2>
-        <h2>Runtime: {res.runtime} minutes</h2>
+        <h2 className="text-lg">{formatedReleaseDate}</h2>
+        <h2>Runtime: {formatedRuntime()}</h2>
         <h2>
           Genres:<> </>
           {res.genres.map(
@@ -58,6 +116,9 @@ export default async function MovieDetail({ params }) {
             )
           )}
         </h2>
+        <div className="mt-1">
+          <Rating rating={voteRatingNumber} />
+        </div>
         <h2 className="text-sm bg-green-600 inline-block my-2 py-2 px-4 rounded-md">
           {res.status}
         </h2>

336d213daf66c3c7c414a0da278c733fce29c7f0
jvcenedeze <jvcenedeze@hotmail.com>
2023-09-16 18:51:56 -0300

    feat: loading state

---
+++ app/[movie]/loading.tsx
@@ -0,0 +1,17 @@
+export default function LoadingMovie() {
+  return (
+    <div className="flex-1">
+      <div className="bg-slate-200 h-7 w-3/4 sm:w-2/5 mb-2 rounded"></div>
+      <div className="bg-slate-200 h-5 min-w-36 w-1/12 mb-2 rounded"></div>
+      <div className="bg-slate-200 h-4 min-w-36 w-2/12 mb-2 rounded"></div>
+      <div className="bg-slate-200 h-4 w-3/4 sm:w-2/5 mb-2 rounded"></div>
+      <div className="bg-slate-200 h-5 min-w-36 w-2/12 mb-2 rounded"></div>
+      <div className="bg-slate-200 h-4 min-w-36 w-2/12 mb-2 rounded"></div>
+      <div className="bg-slate-200 h-9 w-24 mb-2 rounded"></div>
+      <div className="bg-slate-200 aspect-2/1 my-6 rounded"></div>
+      <div className="bg-slate-200 h-4 w-full mb-2 rounded"></div>
+      <div className="bg-slate-200 h-4 w-full mb-2 rounded"></div>
+      <div className="bg-slate-200 h-4 min-w-36 w-2/5 mb-2 rounded"></div>
+    </div>
+  );
+}
